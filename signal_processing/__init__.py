51e71fd6d4e0519e0f8cf262b7b1bcc0e3c7020d
simonarocco <#19992022Engineer>
2022-01-17 18:33:43 -0500

    adding new pan tompkins function

--- signal_processing/__init__.py
+++ signal_processing/__init__.py
@@ -1,5 +1,5 @@
 from .custom_processing import downsample_signal, remove_baseline_wander,\
     apply_wavelet_reconstruction_denoising, plot_freq_spectrum, remove_baseline_wander, plot_ecg,\
     apply_butter_low_pass, process_hardware_ecg, remove_mean, clean_ecg_signal
-from .feature_extraction import apply_pan_tompkins, compute_snr, compute_mean_ptp, compute_qrs_ratio,\
+from .feature_extraction import apply_pan_tompkins, apply_pan_tompkins_old, compute_snr, compute_mean_ptp, compute_qrs_ratio,\
     compute_power, compute_max_psd_freq
